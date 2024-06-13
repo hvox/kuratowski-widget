e487201b25e0a97a18fe9f8e97f9156171f0fe8e
Uladzislau Khamkou <kham@tuta.io>
2024-06-14 00:18:21 +0300

    Add transparency setting

--- Main.gd
+++ Main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+@onready var sprite = $Sprite
 @onready var window: Window = get_window()
+var opacity_delta = 0.05
 var window_size: Vector2i = Vector2(128, 128)
 var prev_mouse_position: Vector2i
 
@@ -11,6 +13,15 @@ func _ready():
 
 func _process(delta):
 	var mouse_position = DisplayServer.mouse_get_position()
-	if Input.get_mouse_button_mask() & MOUSE_BUTTON_LEFT:
+	var mouse_button_mask = Input.get_mouse_button_mask()
+	if mouse_button_mask & MOUSE_BUTTON_LEFT:
 		window.position += mouse_position - prev_mouse_position
 	prev_mouse_position = mouse_position
+
+func _input(event):
+	if not event is InputEventMouseButton:
+		return
+	if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+		sprite.modulate.a = clamp(sprite.modulate.a + opacity_delta, 0, 1)
+	if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+		sprite.modulate.a = clamp(sprite.modulate.a - opacity_delta, 0, 1)
