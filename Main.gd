9d3503364d146f5c85c0ffa5c8b9291958dcc94b
Uladzislau Khamkou <kham@tuta.io>
2024-06-13 23:55:18 +0300

    Improve mouse support

--- Main.gd
+++ Main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var window: Window = get_window()
 var window_size: Vector2i = Vector2(128, 128)
+var prev_mouse_position: Vector2i
 
 
 func _ready():
@@ -9,5 +10,7 @@ func _ready():
 
 
 func _process(delta):
+	var mouse_position = DisplayServer.mouse_get_position()
 	if Input.get_mouse_button_mask() & MOUSE_BUTTON_LEFT:
-		window.position = DisplayServer.mouse_get_position() - window_size / 2
+		window.position += mouse_position - prev_mouse_position
+	prev_mouse_position = mouse_position
